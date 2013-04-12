ef4c76616ec696314631f68028923553459a7f7c
ranhow.chen <ranhow.chen@gmail.com>
2013-04-12 23:39:09 +0800

    Add context
    	modified:   plugins/net.sf.e4ftrace.ui/src/net/sf/e4ftrace/ui/handler/RawHandler.java

--- plugins/net.sf.e4ftrace.ui/src/net/sf/e4ftrace/ui/handler/RawHandler.java
+++ plugins/net.sf.e4ftrace.ui/src/net/sf/e4ftrace/ui/handler/RawHandler.java
@@ -9,6 +9,7 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.MContext;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 
@@ -20,6 +21,8 @@ public class RawHandler {
 	@Inject @Optional private MWindow window;
 	
 	@Inject @Optional private MApplication application;
+	
+	@Inject @Optional private MContext context;
 	
 	@Inject private FtraceParser tracer;
 	
